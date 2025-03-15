70b7fa9f917f7a027fff848576339dd7dfebb326
ndrnmnk <92930193+ndrnmnk@users.noreply.github.com>
2025-03-15 22:01:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,19 @@
+from backend.BaseAddon import BaseAddon
+from PyQt5.QtWidgets import QLabel
+
+
+class ExampleAddonClassName(BaseAddon):
+	def __init__(self, ui):
+		super().__init__(ui)
+		print("example addon init")
+
+	def on_options(self):
+		return QLabel("label")
+	
+	def on_delete(self):
+		print("why do you deleet me(")
+		return True
+
+
 def run(ui):
-  print("I AM NOT USELESS!")
+	return ExampleAddonClassName(ui)
